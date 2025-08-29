0017d5965b352318f997b456bf259b2d351311cf
edup92 <edup92mail@gmail.com>
2025-08-29 14:36:41 +0000

    up

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+# Whats inside
+
+- Creates github oidc role with limited access to project and env named resources
+- Creates tfstate s3 bucket with versioning, encryption and access only to role
+- Creates ec2 keypair for project
+- Add one or more public domains from the project to Route 53 (optional)
+
 # Usage instructions
 
 ## 1) Login in AWS CloudShell (login in aws region you want to bootstrap)
--- script.sh
+++ script.sh
@@ -4,14 +4,25 @@
 
 read -p "Enter project name: " projectname
 read -p "Enter environment (ej: dev, prod): " projectenv
+read -p "Do you want to add project domain/s to route53 (public domain/s) (optional) ? (y/n): " add_domains
+
+if [[ "$add_domains" =~ ^[yY]$ ]]; then
+  echo "Enter one or more domains separated by spaces (only public domains):"
+  read -a projectdomains
+  echo "Domains to be created: ${projectdomains[@]}"
+fi
+
 read -p "Enter github account name: " ghaccount
 read -p "Enter github repo name: " ghrepo
 
-# Names
+# Variables
 
 role_name="${projectname}-role-${projectenv}-bootstrap"
 policy_name="${projectname}-policy-${projectenv}-bootstrap"
 s3_name="${projectname}-s3-${projectenv}-bootstrap"
+keypair_name="${projectname}-keypair"
+keypair_file="${key_name}.pem"
+declare -A hosted_zone_ids
 
 # Create oidc
 
@@ -136,6 +147,51 @@ fi
 
 rm s3_policy.json
 
+# Keypair
+
+if aws ec2 describe-key-pairs --key-names "$keypair_name" >/dev/null 2>&1; then
+  echo "Key pair exists, skipping"
+else
+  aws ec2 create-key-pair --key-name "$keypair_name" --query "KeyMaterial" --output text > "$keypair_file"
+
+fi
+
+# Domains
+
+if [[ -n "${projectdomains[*]}" ]]; then
+  for domain in "${projectdomains[@]}"; do
+    hz_id=$(aws route53 list-hosted-zones-by-name --dns-name "$domain." \
+      --query "HostedZones[?Name=='$domain.'] | [?Config.PrivateZone==\`false\`].Id" \
+      --output text | head -n 1)
+
+    if [[ -n "$hz_id" ]]; then
+      echo "Hosted zone for $domain exists, skipping"
+    else
+      hosted_zone_id=$(aws route53 create-hosted-zone \
+        --name "$domain" \
+        --caller-reference "$(date +%s)-$domain" \
+        --query "HostedZone.Id" \
+        --output text)
+      hosted_zone_id="${hosted_zone_id##*/}"
+      hosted_zone_ids["$domain"]="$hosted_zone_id"
+      echo "Hosted zone for $domain created."
+    fi
+  done
+fi
+
+# Echos
+
 echo "S3 name: $s3_name"
 echo "Role name: $role_name"
+echo "Keypair location: $(realpath "$keypair_file") PLEASE DOWNLOAD PEM"
+if [[ -n "${projectdomains[*]}" ]]; then
+  for domain in "${projectdomains[@]}"; do
+    ns_servers=$(aws route53 get-hosted-zone --id "${hosted_zone_ids[$domain]}" \
+      --query "DelegationSet.NameServers" --output text)
+    echo "Domain $domain added to Route53."
+    echo "Nameservers:"
+    echo "$ns_servers"
+    echo ""
+  done
+fi
 
