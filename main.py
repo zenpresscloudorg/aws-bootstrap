a85f5ffb68be509b37169249bc906d02dc8eab4d
edup92 <edup92mail@gmail.com>
2025-09-03 23:57:16 +0000

    d

--- main.py
+++
@@ -1,8 +0,0 @@
-from validator import validate_vars
-
-def main():
-    vars_data = validate_vars()
-    # Aquí puedes seguir usando vars_data como quieras
-    print("Variables validadas correctamente:")
-    for k, v in vars_data.items():
-        print(f"{k}: {v}")
