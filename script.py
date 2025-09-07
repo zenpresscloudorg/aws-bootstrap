1f183582379e6530586a87188d556819526d456c
edup92 <edup92mail@gmail.com>
2025-09-07 22:30:28 +0000

    v

--- script.py
+++ script.py
@@ -379,22 +379,31 @@ list_natwg = ec2.describe_instances(Filters=[
     {"Name": "tag:Name", "Values": [instance_name]},
     {"Name": "instance-state-name", "Values": ["pending", "running", "stopping", "stopped"]}
 ])["Reservations"]
-userdata_script = """#!/bin/bash
+private_subnet_cidrs = []
+for subnet in list_subnets:
+    name = None
+    for tag in subnet.get("Tags", []):
+        if tag["Key"] == "Name":
+            name = tag["Value"]
+    if name and "-subnet-private-" in name:
+        private_subnet_cidrs.append(subnet["CidrBlock"])
+advertise_routes = ",".join(private_subnet_cidrs)
+userdata_script = f"""#!/bin/bash
 sudo yum update -y
+sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://pkgs.tailscale.com/stable/amazon-linux/2023/tailscale.repo
-yum install -y iptables-services yum-utils tailscale
+sudo yum install -y iptables-services tailscale
 sysctl -w net.ipv4.ip_forward=1
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 echo 'net.ipv6.conf.all.forwarding = 1' >> /etc/sysctl.conf
 sudo sysctl -p /etc/sysctl.conf
-ETH_IFACE=$(ip route | grep default | awk '{print $5}')
+ETH_IFACE=$(ip route | grep default | awk '{{print $5}}')
 iptables -t nat -A POSTROUTING -o $ETH_IFACE -j MASQUERADE
 service iptables save
 systemctl enable iptables
 systemctl start iptables
 sudo systemctl enable --now tailscaled
-sudo tailscale up
-sudo tailscale set --auth-key=vpc_subnet_private_tskey --hostname=instance_name --advertise-routes=192.0.2.0/24
+sudo tailscale up --auth-key={vpc_subnet_private_tskey} --hostname={instance_name} --advertise-routes={advertise_routes}
 """
 
 if list_igws:
