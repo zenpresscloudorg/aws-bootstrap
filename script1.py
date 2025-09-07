b47a0fe09e49e322dbaeb92e51309a1308724f42
edup92 <edup92mail@gmail.com>
2025-09-07 23:32:32 +0000

    k

--- script1.py
+++ script1.py
@@ -13,7 +13,7 @@ session = boto3.session.Session()
 s3 = boto3.client("s3")
 ec2 = boto3.client("ec2")
 
-def github_oidc_provider_exists(iam):
+def list_oidc_provider(iam):
     """
     Returns True if the GitHub OIDC provider for GitHub Actions exists, False otherwise.
     """
@@ -27,29 +27,25 @@ def github_oidc_provider_exists(iam):
     return False
 
 
-def create_github_oidc_provider(iam):
+def create_oidc_provider(iam):
     """
     Creates the GitHub OIDC provider in the AWS account and returns its ARN.
     """
     OIDC_URL = "https://token.actions.githubusercontent.com"
     CLIENT_ID = "sts.amazonaws.com"
     THUMBPRINT = "6938fd4d98bab03faadb97b34396831e3780aea1"  # GitHub Actions root CA thumbprint
-    resp = iam.create_open_id_connect_provider(
+    iam.create_open_id_connect_provider(
         Url=OIDC_URL, ClientIDList=[CLIENT_ID], ThumbprintList=[THUMBPRINT]
     )
-    return resp["OpenIDConnectProviderArn"]
 
 # Main
 
 def main():
-    arn = github_oidc_provider_exists(iam)
-    print(arn)
-    #if arn:
-    #    print(f"OIDC provider exists: {arn}")
-    #else:
-     #   arn = create_github_oidc_provider(iam)
-    #    print(f"OIDC provider created: {arn}")
+    if list_oidc_provider(iam):
+        print("OIDC provider for GitHub Actions already exists.")
+    else:
+        create_oidc_provider(iam)
+        print(f"OIDC provider created")
 
 if __name__ == "__main__":
     main()
-
