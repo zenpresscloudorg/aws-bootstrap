019b92567ac5a3190c844804e7f9e0d4615b9863
zenpresscloud <zenpresscloudmail@gmail.com>
2025-10-20 22:21:54 +0000

    up

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,3 +1,4 @@
 locals {
+  azs = data.aws_availability_zones.available.names
   keypair_name = "${var.project_name}-bootstrap-${var.project_environment}-keypair-main"
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,15 @@ resource "local_file" "keypair_save" {
   file_permission      = "0600"
   directory_permission = "0700"
 }
+
+# VPC
+
+resource "aws_vpc" "main" {
+  cidr_block                     = var.vpc_cidr
+  enable_dns_support             = true
+  enable_dns_hostnames           = true
+  assign_generated_ipv6_cidr_block = var.vpc_ipv6_enable
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
