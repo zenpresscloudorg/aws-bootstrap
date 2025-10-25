3eaa7a459327c9ea977c048314a4ea0f94d1f5e3
zenpresscloud <zenpresscloudmail@gmail.com>
2025-10-25 00:27:50 +0000

    up

--- main.tf
+++ main.tf
@@ -5,10 +5,3 @@ resource "aws_route53_zone" "public" {
   name = var.hostedzone_public
 }
 
-resource "aws_route53_zone" "private" {
-  name = var.hostedzone_private
-  vpc {
-    vpc_id     = aws_vpc.main.id
-    vpc_region = data.aws_region.current.name
-  }
-}
